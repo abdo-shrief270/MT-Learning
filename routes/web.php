81d11361c3123e34da8c5f7a7a53bc928211d17a
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2025-01-22 09:39:00 +0200

    Add Mails Routes

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Vormkracht10\FilamentMails\Controllers\MailDownloadController;
 use Vormkracht10\FilamentMails\Controllers\MailPreviewController;
 use Illuminate\Support\Facades\Route;
 Route::get('/not-activated', \App\Filament\Pages\AccountNotActivated::class)->name('account-not-activated')->middleware(\App\Http\Middleware\VerifyActivationUser::class);
-Route::get('mails/{mail}/preview', MailPreviewController::class)->name('mails.preview');
-Route::get('mails/{mail}/attachment/{attachment}/{filename}', MailDownloadController::class)->name('mails.attachment.download');
+Route::get('mails/{mail}/preview', MailPreviewController::class)->name('filament.admin.mails.preview');
+Route::get('mails/{mail}/attachment/{attachment}/{filename}', MailDownloadController::class)->name('filament.admin.mails.attachment.download');
